6859133e8bc2108ea27641d924d5668d2186de84
Rudra Diyora <hornedragnorock@gmail.com>
2023-01-13 21:38:51 -0600

    There is an error in this one(popout.js)

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-async function fetchData() {
-    // this is new
+// runs everytime we open up a new website
+const generateHTML = (pageName) => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Study Block</title>
+    </head>
+    <body>
+        <div>
+            <h2>Study Block</h2>
+            <ul>
+                <li id = 'concert'></li>
+            </ul>
+        </div>
+        
+    </body>
+    <script src = "script.js"></script>
+    </html>`
+};
+const generateCSS = () => {
+    return ``
+};
+
+
+switch (window.location.hostname) {
+    case "www.youtube.com":
+        document.body.innerHTML =  generateHTML("YouTube");
+        break;
+    case "www.facebook.com":
+        alert('THis is YOUTUBE')
+        break;
 }
